e989eadbe11feb65b95f62e13d8430c834f4ce05
Hajer Almatari <hajer.almatari@gmail.com>
2022-07-03 23:36:39 +0300

    add files again

--- lib/widgets/storage.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get_storage/get_storage.dart';
-import 'package:get/get.dart';
-import 'change_lang.dart';
-
-
-final box = GetStorage();
-MyController myController = Get.put(MyController());
